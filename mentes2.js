2672f214929d3f945f64355c0e294f231c6ac1de
BuscaProfesores Chile <buscaprofesores@users.noreply.github.com>
2017-05-24 16:05:32 -0400

    Update mentes2.js

--- mentes2.js
+++ mentes2.js
@@ -274,21 +274,7 @@ var treeData = [
               ,{  
                 "name":   "Customer Journey",
                  "url": "https://www.youtube.com/v/OUWwZcVA-mM"
-              }
-             ,{  
-                "name":   "Social Studio",
-                 "url": "https://www.youtube.com/v/27P6am1sIFQ"
               },
-              {  
-                "name":   "Best Email Practices",
-                 "url": "https://www.youtube.com/v/UqRGfbPsTAI"
-              }
-              ,
-              {  
-                "name":   "Social Best Practices for Events",
-                 "url": "https://www.youtube.com/v/hDjqBEy-a4M"
-              }
-               ,
               {  
                 "name":   "Deliverability Guide to the Inbox",
                  "url": "https://www.youtube.com/v/NUEt3cZmSTo"
@@ -298,40 +284,7 @@ var treeData = [
                 "name":   "Achieve List Growth Results",
                  "url": "https://www.youtube.com/v/64ERoj3wpxw"
               },
-              
-    {
-    "name":    "DonorsChoose.org Customer Success"
-     ,"url":   "https://www.youtube.com/v/v66RVWKwO2k"
-    },
-
-    { "name":    "Stanley Black & Decker Customer Success"
-       ,"url":   "https://www.youtube.com/v/bSSpSnfjI28"
-    } , 
-
-    { "name":    "Live Nation Customer Success"
-       ,"url":   "https://www.youtube.com/v/rfpcOoRv7Ww"
-    },
-    {
-     "name":    "Unilever Customer Success"
-      ,"url":   "https://www.youtube.com/v/8EssPctyL0M"
-    },
-
-     {"name":    "Royal Bank of Canada Customer Success"
-      ,"url":    "https://www.youtube.com/v/lOqvGs8r0MI"
-     },
-    {
-      "name":   "Philips Customer Success"
-       ,"url":   "https://www.youtube.com/v/_UrGfHAshZk"
-    }
-
-    ,{ "name":    "Microsoft Customer Success"
-       ,"url":   "https://www.youtube.com/v/or16DB2qcXk"
-    },
-
-     {"name":    "Sony Playstation Customer Success"
-      ,"url":  "https://www.youtube.com/v/Pg08zNAqYDU"
-    },
-
+    
      {"name":    "Minacs Solutions Customer Success"
        ,"url":  "https://www.youtube.com/v/V08WobkXA3Y"
     },
