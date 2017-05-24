8fc05e9ad94dec6ff797e783f506d093968bcb4b
BuscaProfesores Chile <buscaprofesores@users.noreply.github.com>
2017-05-24 18:07:22 -0400

    Update mentes2.js

--- mentes2.js
+++ mentes2.js
@@ -12,7 +12,7 @@ var treeData = [
             "_children": [
               {  
                 "name":   "Pablo Valenzuela",
-                 "url": "https://www.youtube.com/v/watch?v=ELk5WWI4DuQ"
+                 "url": "https://www.youtube.com/v/ELk5WWI4DuQ"
               }
  
             ]
@@ -22,7 +22,7 @@ var treeData = [
             ,"_children": [
              {  
                 "name":   "Ricardo Baeza",
-                 "url": "https://www.youtube.com/v/watch?v=HBtgHzXfcdg"
+                 "url": "https://www.youtube.com/v/HBtgHzXfcdg"
               },
 
               {  
